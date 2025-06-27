18099fb682a37aeb86f97b69e0c592574b2d7d39
Dr Data <135477108+DrDataYE@users.noreply.github.com>
2025-06-27 05:29:06 +0300

    Update order_type.dart

--- lib/src/core/enums/order_type.dart
+++ lib/src/core/enums/order_type.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:master_key/src/core/core.dart';
+
+
 enum OrderType {
   byName,
   byCreationTime,
   byLastModified;
 
+  String getName(BuildContext context) => switch (this) {
+    OrderType.byName => context.lang.sortByName,
+    OrderType.byCreationTime => context.lang.sortByCreation,
+    OrderType.byLastModified => context.lang.sortByLastModified,
+  };
 
   factory OrderType.fromIndex(int? index) {
     return OrderType.values.firstWhere(
